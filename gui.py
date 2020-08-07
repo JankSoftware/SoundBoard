c1760d89e2bddaa86be8219339bfb6f862cf9014
brainslushee <austindsmith15@gmail.com>
2020-08-07 09:42:05 -0600

    Refresh and Remove functionality started

--- gui.py
+++ gui.py
@@ -15,10 +15,12 @@ from playsound import *
 
 database_path = "c:\\users\\the goof troop\\desktop\\sound effects\\sounds.db"
 
+#Creates window. Frame helps for refresh.
 root = Tk()
+frame = Frame(root)
 
 #Iterator to help with button row stuff
-i = 0
+x = 0
 
 def create_connection(path):
     connection = None
@@ -65,16 +67,16 @@ def buttonClick(name):
     for row in db.execute(f"SELECT * FROM sounds WHERE name='{name}'"):
         playsound(row[1])
 
-def initialize_buttons(row):
+def initialize_buttons(row, index):
     name = row[0]
-    newButton = Button(root, text=name, bg="black", fg="white", font="Impact", height=5, width=25, command = lambda: buttonClick(name))
-    newButton.grid(row=i, column=1)
+    newButton = Button(frame, text=name, bg="black", fg="white", font="Impact", height=5, width=25, command = lambda: buttonClick(name))
+    newButton.grid(row=index, column=1)
 
 def createButton():
     answer = tkinter.simpledialog.askstring("New button", "Enter a name: ")
     currdir = os.getcwd()
     file = tkinter.filedialog.askopenfilename(parent=root, initialdir=currdir, title='Please select a directory')
-    newButton = Button(root, text=answer, bg="black", fg="white", font="Impact", height=5, width=25, command = lambda: buttonClick(answer))
+    newButton = Button(frame, text=answer, bg="black", fg="white", font="Impact", height=5, width=25, command = lambda: buttonClick(answer))
     newButton.grid()
     add_to_database(answer, file)
     read_database()
@@ -112,33 +114,40 @@ def list_buttons():
 #MAKES AN EXTRA WOO CAUSE ROWS SHIFT AND EVERYTHING COVERS ITSELF
 def refreshButton():
     i = 0
+    for widget in frame.winfo_children():
+        widget.destroy()
+    #homeButtons()
     conn = create_connection(database_path)
     db = conn.cursor()
     for row in db.execute('SELECT * FROM sounds ORDER BY name'):
-        initialize_buttons(row)
+        initialize_buttons(row, i)
         i = i+1
     conn.commit()
     conn.close()
-
 #Creates button widget
-add = Button(root, text="Add", bg="green", fg="white", font="Impact", height=5, width=25, command=createButton)
-clear = Button(root, text="Clear", bg="red", fg="white", font="Impact", height=5, width=25, command=clear_database)
-remove = Button(root, text="Remove", bg="orange", height=5, fg="white", font="Impact", width=25, command=list_buttons)
-refresh = Button(root, text="Refresh", bg="blue", fg="white", font="Impact", height=5, width=25, command=refreshButton)
+add = Button(frame, text="Add", bg="green", fg="white", font="Impact", height=5, width=25, command=createButton)
+clear = Button(frame, text="Clear", bg="red", fg="white", font="Impact", height=5, width=25, command=clear_database)
+remove = Button(frame, text="Remove", bg="orange", height=5, fg="white", font="Impact", width=25, command=list_buttons)
+refresh = Button(frame, text="Refresh", bg="blue", fg="white", font="Impact", height=5, width=25, command=refreshButton)
 
 
 #Inserts button widget
-add.grid(row=0, column=0)
-clear.grid(row=1, column=0)
-remove.grid(row=2, column=0)
-refresh.grid(row=3, column=0)
+def homeButtons():
+    add.grid(row=0, column=0)
+    clear.grid(row=1, column=0)
+    remove.grid(row=2, column=0)
+    refresh.grid(row=3, column=0)
+
 #Creates database and initial buttons
 create_database()
 conn = create_connection(database_path)
 db = conn.cursor()
 for row in db.execute('SELECT * FROM sounds ORDER BY name'):
-    initialize_buttons(row)
-    i = i+1
+    initialize_buttons(row, x)
+    x = x+1
 
+#Packs the frame
+frame.grid()
+homeButtons()
 #Main loop
 root.mainloop()
